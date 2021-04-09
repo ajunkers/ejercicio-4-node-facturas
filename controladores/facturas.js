a11f0ddd12f44e0d3dbe47253d5019cd9da892bf
Andrea Vidal <11.andrea.v.p@gmail.com>
2021-04-09 13:25:42 +0200

    get facturas gastos/ingresos y get factura/:id

--- controladores/facturas.js
+++ controladores/facturas.js
@@ -3,6 +3,18 @@ const facturasJSON = require("../utils/datos");
 
 const getFacturas = () => facturasJSON;
 
+const getFacturasTipos = tipo => {
+  const facturas = facturasJSON.then(factura => factura.filter(factura => factura.tipo === tipo));
+  return (facturas);
+};
+
+const getFactura = id => {
+  const factura = facturasJSON.then(factura => factura.find(factura => factura.id === id));
+  return (factura);
+};
+
 module.exports = {
   getFacturas,
+  getFacturasTipos,
+  getFactura
 };
--- rutas/facturas.js
+++ rutas/facturas.js
@@ -1,16 +1,44 @@
 const express = require("express");
 const { checkSchema, check, validationResult } = require("express-validator");
 const { generaError } = require("../utils/errores");
-const { getFacturas, getFactura } = require("../controladores/facturas");
+const { getFacturas, getFacturasTipos, getFactura } = require("../controladores/facturas");
 
 const router = express.Router();
 router.get("/", (req, res, next) => {
   const facturas = getFacturas();
   facturas.then(factura => res.json(
     {
-      datos: factura,
-      total: factura.length
+      total: factura.length,
+      datos: factura
     }
+  ));
+});
+
+router.get("/ingresos", (req, res, next) => {
+  const facturas = getFacturasTipos("ingreso");
+  facturas.then(factura => res.json(
+    {
+      total: factura.length,
+      datos: factura
+    }
+  ));
+});
+
+router.get("/gastos", (req, res, next) => {
+  const facturas = getFacturasTipos("gasto");
+  facturas.then(factura => res.json(
+    {
+      total: factura.length,
+      datos: factura
+    }
+  ));
+});
+
+router.get("/factura/:id", (req, res, next) => {
+  const idFactura = +req.params.id;
+  const factura = getFactura(idFactura);
+  factura.then(itemFactura => res.json(
+    itemFactura
   ));
 });
 
